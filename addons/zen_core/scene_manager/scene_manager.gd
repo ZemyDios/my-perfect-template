5ee9f8b484e854c20b3e3b270da1b6bedfeb6e22
ZemyDios <136511041+ZemyDios@users.noreply.github.com>
2025-06-06 17:14:56 +0200

    feat: Use SceneManager as Bootstrapper

--- addons/zen_core/scene_manager/scene_manager.gd
+++ addons/zen_core/scene_manager/scene_manager.gd
@@ -1,12 +1,31 @@
 extends Node
 
-const MAIN_SCENE = preload("res://main.tscn")
+const MAIN_SCENE_PATH = preload("res://addons/zen_core/scene_manager/main.tscn")
 
-@onready var scene_handler: SceneHandler
+var scene_handler: SceneHandler
 
 
 func _ready() -> void:
-	print("[ZEN] - " + str(self.name) + " loaded.")
-	scene_handler = get_tree().current_scene
+	print("[ZEN] - SceneManager bootstrapping...")
 
-	scene_handler.add_scene_to_ui(MAIN_SCENE.instantiate())
+	# Get scene instanciated by engine
+	var current_scene := get_tree().current_scene
+
+	# Instanciate ZEN scene handler
+	scene_handler = MAIN_SCENE_PATH.instantiate()
+	# Add scene handler scene to root
+	get_tree().root.add_child.call_deferred(scene_handler)
+
+	# Wait SceneHandler to finish
+	await get_tree().process_frame
+
+	# Remove original scene
+	get_tree().root.remove_child(current_scene)
+	# Add original scene inside SceneHandler
+	scene_handler.add_scene_to_ui(current_scene)
+
+	# Assign SceneHandler as current scene
+	get_tree().current_scene = scene_handler
+	print(get_tree().current_scene)
+
+	print("[ZEN] - " + str(self.name) + " loaded.")
--- addons/zen_core/zen_core.gd
+++ addons/zen_core/zen_core.gd
@@ -1,27 +1,12 @@
 @tool
 extends EditorPlugin
 
-const MAIN_SCENE_PATH = "res://addons/zen_core/scene_manager/main.tscn"
-
 
 func _enable_plugin() -> void:
 	# Add Singletons
 	add_autoload_singleton("SceneManager", "res://addons/zen_core/scene_manager/scene_manager.gd")
 
-	# Set main.tscn as main scene
-	if FileAccess.file_exists(MAIN_SCENE_PATH):
-		ProjectSettings.set_setting("application/run/main_scene", MAIN_SCENE_PATH)
-		ProjectSettings.save()
-		print("[ZEN] - main.tscn set as main scene.")
-	else:
-		push_error("[ZEN] - main.tscn not found at expected path!")
-
 
 func _disable_plugin() -> void:
 	# Remove singletons
 	remove_autoload_singleton("SceneManager")
-
-	# Clear main scene setting
-	ProjectSettings.set_setting("application/run/main_scene", null)
-	ProjectSettings.save()
-	print("[ZEN] - Removed main scene.")
