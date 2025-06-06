7fe94ca8af458185650b6bc4683eab2044977427
ZemyDios <136511041+ZemyDios@users.noreply.github.com>
2025-06-06 05:03:47 +0200

    feat: create demo main menu

---
+++ main.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_play_button_pressed() -> void:
+	print("TODO: Play")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_config_button_pressed() -> void:
+	print("TODO: Change Scene to Configuration")
