f2dd8cb09bc8e419ad7c310dc4813e6567686aa0
ZemyDios <136511041+ZemyDios@users.noreply.github.com>
2025-06-07 17:05:14 +0200

    feat: Add OverlaysRegistry

--- addons/zen_core/zen_core.gd
+++ addons/zen_core/zen_core.gd
@@ -3,15 +3,18 @@ extends EditorPlugin
 
 const SCENE_MANAGER_PATH = "res://addons/zen_core/scene_manager/scene_manager.gd"
 const TRANSITION_REGISTRY_PATH = "res://addons/zen_core/transitions/transition_registry.gd"
+const OVERLAY_REGISTRY_PATH = "res://addons/zen_core/overlays/overlay_registry.gd"
 
 
 func _enable_plugin() -> void:
 	# Add Singletons
 	add_autoload_singleton("SceneManager", SCENE_MANAGER_PATH)
 	add_autoload_singleton("TransitionRegistry", TRANSITION_REGISTRY_PATH)
+	add_autoload_singleton("OverlayRegistry", OVERLAY_REGISTRY_PATH)
 
 
 func _disable_plugin() -> void:
 	# Remove singletons
 	remove_autoload_singleton("SceneManager")
 	remove_autoload_singleton("TransitionRegistry")
+	remove_autoload_singleton("OverlayRegistry")
