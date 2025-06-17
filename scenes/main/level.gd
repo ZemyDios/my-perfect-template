dc5696c18c7a61af1b8875d2a89dc2d1bc634007
ZemyDios <136511041+ZemyDios@users.noreply.github.com>
2025-06-17 04:09:15 +0200

    fix: Now can complete level 1 time

--- scenes/main/level.gd
+++ scenes/main/level.gd
@@ -3,12 +3,16 @@ extends Node2D
 signal level_completed
 signal level_failed
 
+var completed := false
+
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey:
+	if event is InputEventKey and not completed:
 		if event.keycode == 87:
 			level_completed.emit()
 			SceneManager.change_to("win_screen")
+			completed = true
 		else:
 			level_failed.emit()
 			SceneManager.change_to("lose_screen")
+			completed = true
